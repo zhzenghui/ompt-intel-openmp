2e916e8c9e2f9082e2d68696e37249bf50853aab
John Mellor-Crummey <jmellorcrummey@comcast.net>
2013-05-10 15:17:38 +0000

    add init and fini. reorganize.

---
+++ itt/libomp_oss/src/ompt/ompt.h
@@ -0,0 +1,7 @@
+#include "ompt-event-enum.h"
+#include "ompt-types.h"
+
+
+int ompt_set_callback(ompt_event_t evid, ompt_callback_t cb);
+int ompt_get_callback(ompt_event_t evid, ompt_callback_t *cb);
+
