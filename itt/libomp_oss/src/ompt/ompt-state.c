68d0da4875f2e98b14a89e252fd21ed5db50e020
John Mellor-Crummey <jmellorcrummey@comcast.net>
2013-05-11 05:10:15 +0000

    add default state.

--- itt/libomp_oss/src/ompt/ompt-state.c
+++ itt/libomp_oss/src/ompt/ompt-state.c
@@ -1,8 +1,13 @@
 #include "ompt-openmp-interface.h"
-
 #include "ompt-internal.h"
 
 _OMP_EXTERN ompt_state_t ompt_get_state()
 {
-   return ompt_get_state_internal();
+   ompt_state_t thread_state = ompt_get_state_internal();
+
+   if (thread_state == ompt_state_undefined) {
+     thread_state = ompt_state_work_serial;
+   }
+
+   return thread_state;
 }
