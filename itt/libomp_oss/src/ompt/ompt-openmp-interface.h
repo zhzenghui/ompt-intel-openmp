f1a51c12d96a303fa04c466dbb88225670b41a49
John Mellor-Crummey <jmellorcrummey@comcast.net>
2013-05-12 06:05:10 +0000

    add implementation for ompt_get_runtime_version.

--- itt/libomp_oss/src/ompt/ompt-openmp-interface.h
+++ itt/libomp_oss/src/ompt/ompt-openmp-interface.h
@@ -2,9 +2,12 @@
 
 extern ompt_state_t ompt_get_state_internal(ompt_wait_id_t *ompt_wait_id)
 ompt_data_t *ompt_get_thread_data_internal(void);
-
 int ompt_get_runtime_version_internal(char *buffer, int length);
 
 
 ompt_parallel_id_t ompt_get_parallel_id_internal(int ancestor_level);
 void *ompt_get_parallel_function_internal(int ancestor_level);
+
+ompt_data_t *ompt_get_task_data(int ancestor_level);
+ompt_frame_t *ompt_get_task_frame(int ancestor_level);
+void *ompt_get_task_function(int ancestor_level);
