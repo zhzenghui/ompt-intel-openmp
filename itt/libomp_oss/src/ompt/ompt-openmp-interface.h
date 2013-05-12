ec899433c828378d11291721177492ec6b06c20f
John Mellor-Crummey <jmellorcrummey@comcast.net>
2013-05-12 06:08:27 +0000

    correct interface for ompt_get_state.

--- itt/libomp_oss/src/ompt/ompt-openmp-interface.h
+++ itt/libomp_oss/src/ompt/ompt-openmp-interface.h
@@ -1,8 +1,8 @@
 #include "ompt-internal.h"
 
-extern ompt_state_t ompt_get_state_internal(ompt_wait_id_t *ompt_wait_id)
-ompt_data_t *ompt_get_thread_data_internal(void);
-int ompt_get_runtime_version_internal(char *buffer, int length);
+extern ompt_state_t ompt_get_state_internal(ompt_wait_id_t *ompt_wait_id);
+extern ompt_data_t *ompt_get_thread_data_internal(void);
+extern int ompt_get_runtime_version_internal(char *buffer, int length);
 
 
 ompt_parallel_id_t ompt_get_parallel_id_internal(int ancestor_level);
