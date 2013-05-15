2fbac12044a268f63500d8703e0faf7ff548c1ad
John Mellor-Crummey <jmellorcrummey@comcast.net>
2013-05-15 20:26:30 +0000

    lightly comment internal and external interfaces.

--- itt/libomp_oss/src/ompt/ompt-openmp-interface.h
+++ itt/libomp_oss/src/ompt/ompt-openmp-interface.h
@@ -4,17 +4,23 @@
 extern "C" {
 #endif
 
-extern ompt_state_t __ompt_get_state_internal(ompt_wait_id_t *ompt_wait_id);
-extern ompt_data_t *__ompt_get_thread_data_internal(void);
-extern int __ompt_get_runtime_version_internal(char *buffer, int length);
+/* state */
+extern ompt_state_t 		__ompt_get_state_internal(ompt_wait_id_t *ompt_wait_id);
 
+/* thread */
+extern ompt_data_t *		__ompt_get_thread_data_internal(void);
 
-extern ompt_parallel_id_t __ompt_get_parallel_id_internal(int ancestor_level);
-extern void *__ompt_get_parallel_function_internal(int ancestor_level);
+/* parallel region */
+extern ompt_parallel_id_t 	__ompt_get_parallel_id_internal(int ancestor_level);
+extern void *			__ompt_get_parallel_function_internal(int ancestor_level);
 
-extern ompt_data_t *__ompt_get_task_data_internal(int ancestor_level);
-extern void *__ompt_get_task_function_internal(int ancestor_level); 
-extern ompt_frame_t *__ompt_get_task_frame_internal(int ancestor_level);
+/* task */
+extern ompt_data_t *		__ompt_get_task_data_internal(int ancestor_level);
+extern void *			__ompt_get_task_function_internal(int ancestor_level); 
+extern ompt_frame_t *		__ompt_get_task_frame_internal(int ancestor_level);
+
+/* version */
+extern int 			__ompt_get_runtime_version_internal(char *buffer, int length);
 
 #ifdef __cplusplus
 };
