2ead5e9d7661561f6e1bc1942c6b6d3806545cec
John Mellor-Crummey <jmellorcrummey@comcast.net>
2013-06-05 23:07:37 +0000

    add parallel loop with fib call in it. this code is used with hpctoolkit to test ompt unwinding, deferred context assembly, and stack frame elision.

---
+++ test/parloop-for-sampling.c
@@ -0,0 +1,32 @@
+#include <omp.h>
+
+#define N 20
+
+int fib(int n)
+{
+  if(n<2) return n;
+  else
+    return fib(n-1)+fib(n-2);
+}
+
+void g()
+{
+  int i, j;
+for(j = 0; j< 50000; j++) {
+#pragma omp parallel for
+  for(i = 0; i<1; i++) {
+    fib(N+3);
+  }
+}
+}
+
+void f()
+{
+  g();
+}
+
+int main()
+{
+  f();
+  return 0;
+}
