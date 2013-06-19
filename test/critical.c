55f7a0ef62a4e5e48da25af0553637a401180b3d
John Mellor-Crummey <jmellorcrummey@comcast.net>
2013-06-19 22:37:27 +0000

    adjust benchmarks for compactness - for presentation of results on slides.

--- test/critical.c
+++ test/critical.c
@@ -1,42 +1,23 @@
 #include <omp.h>
-
 #include "fib.h"
-
-#define N 10
-
 void g()
 {
-  int i, j;
+  int i;
   omp_lock_t l;
   omp_init_lock(&l); 
-
 #pragma omp parallel 
   {
     #pragma omp master
     {
       #pragma omp critical (it)
-      {
-	fib(42);
-      }
+      { fib(40); }
     }
     #pragma omp for
     for(i = 0; i<100; i++) {
       #pragma omp critical (it)
-      {
-	fib(N);
-      }
+      { fib(10); }
     }
   }
-
 }
-
-void f()
-{
-  g();
-}
-
-int main()
-{
-  f();
-  return 0;
-}
+void f() { g(); }
+int main() { f(); return 0; }
