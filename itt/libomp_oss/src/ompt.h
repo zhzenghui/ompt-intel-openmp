4d8a713a0b4a98a17096f4e8ba2dba7281e0495a
John Mellor-Crummey <jmellorcrummey@comcast.net>
2014-07-01 11:18:20 -0500

    avoid separate ompt-state.h, ompt-fns.h, and ompt-event.h files that were included using macros. using an idiom from Jim Cownie, define a macro to iterate over the entities.

--- itt/libomp_oss/src/ompt.h
+++ itt/libomp_oss/src/ompt.h
@@ -8,6 +8,160 @@
 #include <stdint.h>
 
 
+
+/*****************************************************************************
+ * iteration macros 
+ *****************************************************************************/
+
+#define FOREACH_OMPT_FN(macro) \
+	macro (ompt_enumerate_state ) 	\
+					\
+	macro (ompt_set_callback) 	\
+	macro (ompt_get_callback) 	\
+					\
+	macro (ompt_get_idle_frame) 	\
+	macro (ompt_get_task_frame) 	\
+					\
+	macro (ompt_get_state) 		\
+					\
+	macro (ompt_get_parallel_id) 	\
+	macro (ompt_get_task_id) 	\
+	macro (ompt_get_thread_id)
+
+#define FOREACH_OMPT_STATE(macro) \
+											\
+	/* first */									\
+	macro (ompt_state_first, 0x71)           /* initial enumeration state */	\
+											\
+	/* work states (0..15) */							\
+	macro (ompt_state_work_serial, 0x00)     /* working outside parallel */		\
+	macro (ompt_state_work_parallel, 0x01)   /* working within parallel */		\
+	macro (ompt_state_work_reduction, 0x02)  /* performing a reduction */		\
+											\
+	/* idle (16..31) */								\
+	macro (ompt_state_idle, 0x10)            /* waiting for work */			\
+											\
+	/* overhead states (32..63) */							\
+	macro (ompt_state_overhead, 0x20)        /* overhead excluding wait states */	\
+											\
+	/* barrier wait states (64..79) */						\
+	macro (ompt_state_wait_barrier, 0x40)    /* waiting at a barrier */		\
+	macro (ompt_state_wait_barrier_implicit, 0x41)    /* implicit barrier */	\
+	macro (ompt_state_wait_barrier_explicit, 0x42)    /* explicit barrier */	\
+											\
+	/* task wait states (80..95) */							\
+	macro (ompt_state_wait_taskwait, 0x50)   /* waiting at a taskwait */		\
+	macro (ompt_state_wait_taskgroup, 0x51)  /* waiting at a taskgroup */		\
+											\
+	/* mutex wait states (96..111) */  						\
+	macro (ompt_state_wait_lock, 0x60)       /* waiting for lock */			\
+	macro (ompt_state_wait_nest_lock, 0x61)  /* waiting for nest lock */		\
+	macro (ompt_state_wait_critical, 0x62)   /* waiting for critical */		\
+	macro (ompt_state_wait_atomic, 0x63)     /* waiting for atomic */		\
+	macro (ompt_state_wait_ordered, 0x64)    /* waiting for ordered */		\
+	macro (ompt_state_wait_single, 0x65)     /* waiting for single */		\
+											\
+	/* misc (112..127) */								\
+	macro (ompt_state_undefined, 0x70)       /* undefined thread state */
+
+
+#define FOREACH_OMPT_EVENT(macro)  \
+																				\
+	  /*--- Mandatory Events ---*/																\
+	macro (ompt_event_parallel_begin, ompt_new_parallel_callback_t, 1, ompt_event_parallel_begin_implemented) /* parallel begin */				\
+	macro (ompt_event_parallel_end, ompt_new_parallel_callback_t, 2, ompt_event_parallel_end_implemented) /* parallel end */				\
+																				\
+	macro (ompt_event_task_begin, ompt_new_task_callback_t, 3, ompt_event_task_begin_implemented) /* task begin */						\
+	macro (ompt_event_task_end, ompt_new_task_callback_t, 4, ompt_event_task_end_implemented) /* task destroy */						\
+																				\
+	macro (ompt_event_thread_begin, ompt_thread_type_callback_t, 6, ompt_event_thread_begin_implemented) /* thread begin */					\
+	macro (ompt_event_thread_end, ompt_thread_type_callback_t, 7, ompt_event_thread_end_implemented) /* thread end */					\
+																				\
+	macro (ompt_event_control, ompt_control_callback_t, 8, ompt_event_control_implemented) /* support control calls */					\
+																				\
+	macro (ompt_event_runtime_shutdown, ompt_callback_t, 9, ompt_event_runtime_shutdown_implemented) /* runtime shutdown */					\
+																				\
+	  /*--- Optional Events (blame shifting, ompt_event_unimplemented) ---*/										\
+	macro (ompt_event_idle_begin, ompt_thread_callback_t, 10, ompt_event_idle_begin_implemented) /* begin idle state */					\
+	macro (ompt_event_idle_end, ompt_thread_callback_t, 11, ompt_event_idle_end_implemented) /* end idle state */						\
+																				\
+	macro (ompt_event_wait_barrier_begin, ompt_parallel_callback_t, 12, ompt_event_wait_barrier_begin_implemented) /* begin wait at barrier */		\
+	macro (ompt_event_wait_barrier_end, ompt_parallel_callback_t, 13, ompt_event_wait_barrier_end_implemented) /* end wait at barrier */			\
+																				\
+	macro (ompt_event_wait_taskwait_begin, ompt_parallel_callback_t, 14, ompt_event_wait_taskwait_begin_implemented) /* begin wait at taskwait */		\
+	macro (ompt_event_wait_taskwait_end, ompt_parallel_callback_t, 15, ompt_event_wait_taskwait_end_implemented) /* end wait at taskwait */			\
+																				\
+	macro (ompt_event_wait_taskgroup_begin, ompt_parallel_callback_t, 16, ompt_event_wait_taskgroup_begin_implemented) /* begin wait at taskgroup */	\
+	macro (ompt_event_wait_taskgroup_end, ompt_parallel_callback_t, 17, ompt_event_wait_taskgroup_end_implemented) /* end wait at taskgroup */		\
+																				\
+	macro (ompt_event_release_lock, ompt_wait_callback_t, 18, ompt_event_release_lock_implemented) /* lock release */					\
+	macro (ompt_event_release_nest_lock_last, ompt_wait_callback_t, 19, ompt_event_release_nest_lock_last_implemented) /* last nest lock release */ 	\
+	macro (ompt_event_release_critical, ompt_wait_callback_t, 20, ompt_event_release_critical_implemented) /* critical release */				\
+																				\
+	macro (ompt_event_release_atomic, ompt_wait_callback_t, 21, ompt_event_release_atomic_implemented) /* atomic release */					\
+																				\
+	macro (ompt_event_release_ordered, ompt_wait_callback_t, 22, ompt_event_release_ordered_implemented) /* ordered release */				\
+																				\
+	  /*--- Optional Events (synchronous events, ompt_event_unimplemented) --- */										\
+	macro (ompt_event_implicit_task_begin, ompt_parallel_callback_t, 23, ompt_event_implicit_task_begin_implemented) /* implicit task begin   */		\
+	macro (ompt_event_implicit_task_end, ompt_parallel_callback_t, 24, ompt_event_implicit_task_end_implemented) /* implicit task end  */			\
+																				\
+	macro (ompt_event_initial_task_begin, ompt_parallel_callback_t, 25, ompt_event_initial_task_begin_implemented) /* initial task begin   */		\
+	macro (ompt_event_initial_task_end, ompt_parallel_callback_t, 26, ompt_event_initial_task_end_implemented) /* initial task end  */			\
+																				\
+	macro (ompt_event_task_switch, ompt_task_switch_callback_t, 27, ompt_event_task_switch_implemented) /* task switch */					\
+																				\
+	macro (ompt_event_loop_begin, ompt_new_workshare_callback_t, 28, ompt_event_loop_begin_implemented) /* task at loop begin */				\
+	macro (ompt_event_loop_end, ompt_new_workshare_callback_t, 29, ompt_event_loop_end_implemented) /* task at loop end */					\
+																				\
+	macro (ompt_event_sections_begin, ompt_new_workshare_callback_t, 30, ompt_event_sections_begin_implemented) /* task at sections begin  */		\
+	macro (ompt_event_sections_end, ompt_new_workshare_callback_t, 31, ompt_event_sections_end_implemented) /* task at sections end */			\
+																				\
+	macro (ompt_event_single_in_block_begin, ompt_new_workshare_callback_t, 32, ompt_event_single_in_block_begin_implemented) /* task at single begin*/  	\
+	macro (ompt_event_single_in_block_end, ompt_new_workshare_callback_t, 33, ompt_event_single_in_block_end_implemented) /* task at single end */		\
+																				\
+	macro (ompt_event_single_others_begin, ompt_parallel_callback_t, 34, ompt_event_single_others_begin_implemented) /* task at single begin */		\
+	macro (ompt_event_single_others_end, ompt_parallel_callback_t, 35, ompt_event_single_others_end_implemented) /* task at single end */			\
+																				\
+	macro (ompt_event_workshare_begin, ompt_new_workshare_callback_t, 36, ompt_event_workshare_begin_implemented) /* task at workshare begin */		\
+	macro (ompt_event_workshare_end, ompt_new_workshare_callback_t, 37, ompt_event_workshare_end_implemented) /* task at workshare end */			\
+																				\
+	macro (ompt_event_master_begin, ompt_parallel_callback_t, 38, ompt_event_master_begin_implemented) /* task at master begin */				\
+	macro (ompt_event_master_end, ompt_parallel_callback_t, 39, ompt_event_master_end_implemented) /* task at master end */					\
+																				\
+	macro (ompt_event_barrier_begin, ompt_parallel_callback_t, 40, ompt_event_barrier_begin_implemented) /* task at barrier begin  */			\
+	macro (ompt_event_barrier_end, ompt_parallel_callback_t, 41, ompt_event_barrier_end_implemented) /* task at barrier end */				\
+																				\
+	macro (ompt_event_taskwait_begin, ompt_parallel_callback_t, 42, ompt_event_taskwait_begin_implemented) /* task at taskwait begin */			\
+	macro (ompt_event_taskwait_end, ompt_parallel_callback_t, 43, ompt_event_taskwait_end_implemented) /* task at task wait end */				\
+																				\
+	macro (ompt_event_taskgroup_begin, ompt_parallel_callback_t, 44, ompt_event_taskgroup_begin_implemented) /* task at taskgroup begin */			\
+	macro (ompt_event_taskgroup_end, ompt_parallel_callback_t, 45, ompt_event_taskgroup_end_implemented) /* task at taskgroup end */			\
+																				\
+	macro (ompt_event_release_nest_lock_prev, ompt_wait_callback_t, 46, ompt_event_release_nest_lock_prev_implemented) /* prev nest lock release */		\
+																				\
+	macro (ompt_event_wait_lock, ompt_wait_callback_t, 47, ompt_event_wait_lock_implemented) /* lock wait */						\
+	macro (ompt_event_wait_nest_lock, ompt_wait_callback_t, 48, ompt_event_wait_nest_lock_implemented) /* nest lock wait */					\
+	macro (ompt_event_wait_critical, ompt_wait_callback_t, 49, ompt_event_wait_critical_implemented) /* critical wait */					\
+	macro (ompt_event_wait_atomic, ompt_wait_callback_t, 50, ompt_event_wait_atomic_implemented) /* atomic wait */						\
+	macro (ompt_event_wait_ordered, ompt_wait_callback_t, 51, ompt_event_wait_ordered_implemented) /* ordered wait */					\
+																				\
+	macro (ompt_event_acquired_lock, ompt_wait_callback_t, 52, ompt_event_acquired_lock_implemented) /* lock acquired */					\
+	macro (ompt_event_acquired_nest_lock_first, ompt_wait_callback_t, 53, ompt_event_acquired_nest_lock_first_implemented) /* 1st nest lock acquired */	\
+	macro (ompt_event_acquired_nest_lock_next, ompt_parallel_callback_t, 54, ompt_event_acquired_nest_lock_next_implemented) /* next nest lock acquired*/	\
+	macro (ompt_event_acquired_critical, ompt_wait_callback_t, 55, ompt_event_acquired_critical_implemented) /* critical acquired */			\
+	macro (ompt_event_acquired_atomic, ompt_wait_callback_t, 56, ompt_event_acquired_atomic_implemented) /* atomic acquired */				\
+	macro (ompt_event_acquired_ordered, ompt_wait_callback_t, 57, ompt_event_acquired_ordered_implemented) /* ordered acquired */				\
+																				\
+	macro (ompt_event_init_lock, ompt_wait_callback_t, 58, ompt_event_init_lock_implemented) /* lock init */						\
+	macro (ompt_event_init_nest_lock, ompt_wait_callback_t, 59, ompt_event_init_nest_lock_implemented) /* nest lock init */					\
+	macro (ompt_event_destroy_lock, ompt_wait_callback_t, 60, ompt_event_destroy_lock_implemented) /* lock destruction */					\
+	macro (ompt_event_destroy_nest_lock, ompt_wait_callback_t, 61, ompt_event_destroy_nest_lock_implemented) /* nest lock destruction */			\
+																				\
+	macro (ompt_event_flush, ompt_callback_t, 62, ompt_event_flush_implemented) /* after executing flush */
+
+
+
 /*****************************************************************************
  * data types
  *****************************************************************************/
@@ -39,7 +193,6 @@ typedef struct ompt_frame_s {
 } ompt_frame_t;
 
 
-
 /*****************************************************************************
  * enumerations for thread states and runtime events 
  *****************************************************************************/
@@ -49,8 +202,9 @@ typedef struct ompt_frame_s {
  *---------------------*/
 
 typedef enum {
-#define ompt_state(state, code) state = code,
-#include "ompt-state.h"
+#define ompt_state_macro(state, code) state = code,
+	FOREACH_OMPT_STATE(ompt_state_macro)
+#undef ompt_state_macro
 } ompt_state_t;
 
 
@@ -59,8 +213,9 @@ typedef enum {
  *---------------------*/
 
 typedef enum {
-#define ompt_event(event, callback, eventid, is_impl) event = eventid,
-#include "ompt-event.h"
+#define ompt_event_macro(event, callback, eventid, is_impl) event = eventid,
+	FOREACH_OMPT_EVENT(ompt_event_macro)
+#undef ompt_event_macro
 } ompt_event_t;
 
 
@@ -242,8 +397,6 @@ OMPT_API_FUNCTION(int, ompt_enumerate_state, (
   int *next_state, 
   const char **next_state_name
 ));
-
-
 
 #ifdef  __cplusplus
 };
